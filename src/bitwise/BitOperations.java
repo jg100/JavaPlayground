434f6f5b3189eae85f0e693ea8c9d437658c9f68
Joel Giannelli <jgiannelli13@gmail.com>
2022-06-24 16:30:35 -0700

    get bit operation done

--- src/bitwise/BitOperations.java
+++ src/bitwise/BitOperations.java
@@ -6,11 +6,37 @@ public class BitOperations {
         int num = 4;
         int pos = 1;
 
-        System.out.println(set(num,pos));
+        //System.out.println(set(num,pos));
+
+        System.out.println(getBit(2,1));
 
     }
 
+
+    /**
+     * Takes the index and uses 0001 (1) to move the bit to the i-th
+     * location.
+     * Use Bitwise AND to compare the two. If the bit is set 1 is returned.
+     * If the bit is not set then 0 is returned.
+     *
+     * @param num
+     * @param index
+     * @return
+     */
     public static boolean getBit(int num, int index) {
+        //break down each step
+
+        //moving bit. Move bit to the desired index
+        int move = (1 << index);
+
+        //bitwise and operation
+        return (num & move) != 0;
+
+        /**
+         * Can also be written in a single line
+         *
+         * return (num $ (1 << index)) != 0);
+         */
 
 
 
@@ -23,6 +49,9 @@ public class BitOperations {
         num |= (1 << pos);
         return num;
     }
+
+
+    // Clear bit
 
 
 
