a37086ca6cc19132f9be99bf4be5e630a620fd97
Joel Giannelli <jgiannelli13@gmail.com>
2022-06-27 11:02:04 -0700

    working on remove function

--- src/ArrayList/ArrayList.java
+++ src/ArrayList/ArrayList.java
@@ -7,25 +7,21 @@ import java.lang.reflect.Array;
 
 public class ArrayList<T> {
     private T[] arr;
-    private int size;
 
     //Arg constructor
     public ArrayList(T[] arr) {
         this.arr = arr;
-        this.size = arr.length;
     }
 
     //Zero arg constructor, constructs in ArrayList with initial capacity fo 10
     public ArrayList() {
         //Creating new array of Objects but casting into T[]
         this.arr = (T[]) new Object[10];
-        this.size = 10;
     }
 
     //Constructor with initial capacity
     public ArrayList(int capacity) {
         this.arr = (T[]) new Object[capacity];
-        this.size = capacity;
     }
 
     //Copy constructor
@@ -33,17 +29,16 @@ public class ArrayList<T> {
 
     //Behaviors
     public int length() {
-
-        return size;
-
+        return this.arr.length;
     }
 
     public boolean add(T value) {
         try {
-            T[] temp = (T[]) new Object[this.size + 1];
+            T[] temp = (T[]) new Object[this.length() + 1];
             copy(this.arr, temp);
             this.arr = temp;
             this.arr[this.arr.length - 1] = value;
+
             return true;
         } catch (Exception ex) {
             System.out.println(ex);
@@ -51,6 +46,7 @@ public class ArrayList<T> {
         }
     }
 
+    //Private helper function
     private void copy(T[] arrToCopy, T[] targetArray) {
         for(int i = 0; i < targetArray.length; i++) {
             targetArray[i] = arrToCopy[i];
@@ -58,10 +54,26 @@ public class ArrayList<T> {
     }
 
     public boolean remove(int index) {
+        for(int i = 0; i < this.length(); i++) {
+            if(index == i) {
+
+            }
+        }
         return false;
     }
 
     public boolean remove(T value) {
+        for(int i = 0; i < this.length(); i++) {
+            if(this.arr[i].equals(value)) {
+
+            }
+        }
+
+    }
+
+    private void remove(T[] arr, T value) {
+        T[] temp = (T[]) new Object[this.length() - 1];
+
 
     }
 
@@ -69,6 +81,8 @@ public class ArrayList<T> {
     public int indexOf(T value) {
 
     }
+
+
 
 
 
