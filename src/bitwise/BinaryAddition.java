f57ddd8ef793839af53a30c80d92e9715aac1a97
Joel Giannelli <jgiannelli13@gmail.com>
2022-07-07 23:21:08 -0700

    Starting binary addition function

---
+++ src/bitwise/BinaryAddition.java
@@ -0,0 +1,15 @@
+package bitwise;
+
+public class BinaryAddition {
+    public static void main(String[] args) {
+        binaryAddition("11","1");
+        binaryAddition("1010","1011");
+    }
+
+    public static String binaryAddition(String a, String b) {
+
+
+
+
+    }
+}
