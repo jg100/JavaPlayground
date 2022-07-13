dccecfce03a2e38a9ace07fd1db6eec71c42c2cd
Joel Giannelli <jgiannelli13@gmail.com>
2022-07-13 16:30:02 -0700

    Recursive calls placed in for loop

--- src/permutations/ArrayPermutation.java
+++ src/permutations/ArrayPermutation.java
@@ -17,18 +17,21 @@ public class ArrayPermutation {
     public static void backtrack(ArrayList<Integer> numsList) {
         //base case
         if(numsList.size() == 1) { //if there is only one value return to last call
-            System.out.println(numsList);
+            System.out.println("Base case: " + numsList);
             return;
         }
 
-        //recursive call
-        /**
-         * Keep removing the first value until one is reached
-         */
+        // For every number in the list, make a recursive call to get permutation of #s with value removed
+        for(int i = 0; i < numsList.size(); i++) {
+            //remove the element and make recursive call
+            int val = numsList.remove(i);
+            System.out.println(numsList);
+            backtrack(numsList);
+            //add removed number back
+            numsList.add(0,val);
 
-        int val = numsList.remove(0);
-        backtrack(numsList);
-
+            System.out.println("Out of recursive call");
+        }
 
     }
 
