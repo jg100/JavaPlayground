6e337acd474a942fc9e61c295c3ce5a7776e9098
Joel Giannelli <jgiannelli13@gmail.com>
2022-07-13 23:39:39 -0700

    Null pointer exception

--- src/permutations/ArrayPermutation.java
+++ src/permutations/ArrayPermutation.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 
 public class ArrayPermutation {
 
+    static ArrayList<ArrayList<Integer>> permutation = new ArrayList<>();
+
     //testing main method
     public static void main(String[] args) {
         int[] nums = {1,2,3};
@@ -10,7 +12,7 @@ public class ArrayPermutation {
     }
 
     //Return an array list of array lists that contain integers
-    public static ArrayList<ArrayList<Integer>> backtrack(ArrayList<Integer> numsList) {
+    public static ArrayList<Integer> backtrack(ArrayList<Integer> numsList) {
 
         //ArrayList to collect the permutations for a particular instance
 
@@ -18,45 +20,42 @@ public class ArrayPermutation {
         if(numsList.size() == 1) { //if there is only one value return to last call
             //System.out.println("Base case: " + numsList);
             //Returns a list of list with only the final value
-            ArrayList<ArrayList<Integer>> ret = new ArrayList<>();
-            ret.add(numsList);
+            ArrayList<Integer> ret = new ArrayList<>();
+            ret.add(numsList.get(0));
             System.out.println("Base case: " + ret);
             return ret;
         }
 
-        ArrayList<ArrayList<Integer>> permutation = new ArrayList<>();
+
 
         // For every number in the list, make a recursive call to get permutation of #s with value removed
         for(int i = 0; i < numsList.size(); i++) {
+            System.out.println("Iteration start");
+            ArrayList<Integer> temp = new ArrayList<>();
 
-            int val = numsList.remove(i);
-            System.out.println("Value removed: " + val);
-            permutation.add(new ArrayList<Integer>(val));
+            /**
+             * For every value in the list/sublist remove one.
+             *
+             * Take removed and append the result fo the children to it.
+             *
+             * At the end of every iteration
+             */
+            int val = numsList.remove(i); //removing value i
+            temp.add(val);
 
-            //Adding the removed value to the arrayList
-            System.out.println(numsList);
-            System.out.println("Perm array before addition: " + permutation);
-            permutation.addAll(backtrack(numsList));
+            System.out.println("Temp before rec adAll: " + temp);
+            temp.addAll(backtrack(numsList));
 
-            ArrayList<Integer> sp = new ArrayList<>();
-            System.out.println("Perm array before merge: " + permutation);
-            //Combine the arrays into one
-            for(int j = 0; j < permutation.size(); j++) {
-                sp.add(permutation.get(i).get(0)); //Every element is a single valued AL
-            }
-            permutation.clear();
-            permutation.add(sp);
-            System.out.println("Perm array after merge: " + permutation);
+            numsList.add(i, val); //putting removed value back
 
-            //add removed number back
-            numsList.add(i,val);
-
-            //System.out.println(permutation);
+            System.out.println("Temp at end of iteration: " + temp);
         }
 
         //System.out.println(permutation);
-        return permutation;
+        return null;
     }
+
+
 
     public static ArrayList<ArrayList<Integer>> permute(int[] nums) {
         ArrayList<Integer> numsList = new ArrayList<>();
@@ -69,7 +68,8 @@ public class ArrayPermutation {
         }
 
         //Call backtracking algorithm
-        System.out.println(backtrack(numsList));
+        //System.out.println(backtrack(numsList));
+        backtrack(numsList);
         return null; //debug/testing
     }
 }
