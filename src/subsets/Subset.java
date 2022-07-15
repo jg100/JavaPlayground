5fba21ada492f91f65481ba9df91efc8101c424a
Joel Giannelli <jgiannelli13@gmail.com>
2022-07-15 14:55:12 -0700

    subsets problem done

--- src/subsets/Subset.java
+++ src/subsets/Subset.java
@@ -7,23 +7,33 @@ import java.util.List;
 public class Subset {
     public static void main(String[] args) {
         int[] arr = {3,4,5};
+        System.out.println(subsets(arr));
     }
 
-    private List<List<Integer>> subsets(int[] arr) {
+    private static List<List<Integer>> subsets(int[] arr) {
 
         List<List<Integer>> subsets = new ArrayList<>();
         //Adding the empty set
         subsets.add(new ArrayList<>());
 
+        //traverse everything up until the last value
         for(int i = 0; i < arr.length; i++) {
             //Add the current digit as a subset.
+            subsets.add(new ArrayList<>(Arrays.asList(arr[i])));
 
             //traversing the following numbers
             for(int j = i + 1; j < arr.length; j++) {
-
+                subsets.add(Arrays.asList(arr[i], arr[j]));
             }
-
         }
+
+        //Add the whole array as a subset
+        List<Integer> wholeSet = new ArrayList<>();
+        for(int num : arr) {
+            wholeSet.add(num);
+        }
+        subsets.add(wholeSet);
+
 
         return subsets;
     }
