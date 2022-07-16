0377558162d53c9cc5ce61414e85daa9388b02ed
Joel Giannelli <jgiannelli13@gmail.com>
2022-07-15 17:29:15 -0700

    addNode function done

--- src/tress/Tree.java
+++ src/tress/Tree.java
@@ -1,15 +1,32 @@
 package tress;
 
-public class Tree {
-    private Node root;
-    private Node currentNode;
+public class Tree<T> {
+    private Node<T> root;
+    private Node<T> currentNode;
 
-    Tree(Node root) {
+    Tree(Node<T> root) {
         this.root = root;
+        this.currentNode = root;
     }
 
     Tree() {
         this.root = null;
     }
+
+    public void addNode(Node<T> newNode) {
+        //If left or right nodes are empty, new node is attached to correpsonding node.
+        if(currentNode.getV1() == null) {
+            currentNode.setV1(newNode);
+        } else if(currentNode.getV2() == null) {
+            currentNode.setV2(newNode);
+        } else { //at this point. Node is complete. Need to create new.
+            currentNode.getV1().setV1(newNode); //sets the left most nodes child to newNode
+        }
+        //The most recent addition to the tree is set as the current node
+        this.currentNode = newNode;
+
+    }
+
+
 
 }
